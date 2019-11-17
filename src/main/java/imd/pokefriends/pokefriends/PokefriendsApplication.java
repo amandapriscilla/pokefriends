001bbea0c2bf80d80acce3ff42cac6a226eb4050
Amanda Priscilla <amandapriscilla@users.noreply.github.com>
2019-11-17 15:12:02 -0300

    Iniciando projeto PokeFriendsAPI

---
+++ src/main/java/imd/pokefriends/pokefriends/PokefriendsApplication.java
@@ -0,0 +1,13 @@
+package imd.pokefriends.pokefriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokefriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokefriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imd/pokefriends/pokefriends/PokefriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package imd.pokefriends.pokefriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokefriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
