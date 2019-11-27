a2514d1f648da053bfc47cfb34dd4fd189d0ca68
Amanda Priscilla <2341990+amandapriscilla@users.noreply.github.com>
2019-11-27 04:32:44 -0300

    Teste

--- src/main/java/imd/pokefriends/pokefriends/dao/FriendRepository.java
+++ src/main/java/imd/pokefriends/pokefriends/dao/FriendRepository.java
@@ -3,6 +3,8 @@ package imd.pokefriends.pokefriends.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import imd.pokefriends.pokefriends.domain.Friend;
@@ -11,5 +13,6 @@ import imd.pokefriends.pokefriends.domain.User;
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
-	public List<Friend> findByUser(User user);
+	@Query("FROM Friend f WHERE f.user = :user")
+	public List<Friend> findByUser(@Param("user") User user);
 }
